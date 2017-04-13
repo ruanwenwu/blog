c606d85d6c7705cd14eecebfd7a0110eafb350d4
ruanwenwu <379879523@qq.com>
2017-04-14 02:01:02 +0800

    autoupload

--- Application/Demo/Controller/LittleController.class.php
+++ Application/Demo/Controller/LittleController.class.php
@@ -10,5 +10,16 @@ class LittleController extends Controller {
     public function fromMohuToQing(){
         $this->display();
     }
+
+    public function getToken(){
+        // 指定允许其他域名访问
+        header('Access-Control-Allow-Origin:*');
+        // 响应类型
+        header('Access-Control-Allow-Methods:POST');
+        // 响应头设置
+        header('Access-Control-Allow-Headers:x-requested-with,content-type');
+        echo "EAACEdEose0cBABNWZCWgU75jt8eGjeos6QljQD7wSl6bCF4EdkWaP2wzMZBG4bbqTsWgsXtQyaFaXWBXRyBknhh4tkJDZBaRUWu200JBemgacxZBnw54UpyophrlXOAWo1ZAIAHW9CRsDRGjAyyj2j0ktkYLacMQVGh9pYyOOIPA4aoPLCh7cZCFjXqZB9hoQMZD";
+        die;
+    }
     
 }
