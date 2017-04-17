63a72d0dae3e216f263a9ab385c8b53493567a31
ruanwenwu <379879523@qq.com>
2017-04-18 02:01:01 +0800

    autoupload

--- Application/Demo/Controller/LittleController.class.php
+++ Application/Demo/Controller/LittleController.class.php
@@ -18,7 +18,7 @@ class LittleController extends Controller {
         header('Access-Control-Allow-Methods:POST');
         // œÏ”¶Õ∑…Ë÷√
         header('Access-Control-Allow-Headers:x-requested-with,content-type');
-        echo "EAABc73XSakgBAHzOU5kAyNAt22MsyZALRpPnP8GrZA7lle9ZBFowvn4geU2iyFecCKNttZAoJg4MVtnrrJHemcnkXhXURZBQlDU58xOySCqZAbYFZCzH8rw1ggJV8gZAZB3WRCHZA8YZAbOOx36nIHTI7AW1qqB6ouyxqknJ8SDVMUaf5oXVWN7opmZAu2p0bOb2SYpYnJHmHnRoaAZDZD";
+        echo "EAABc73XSakgBAHSWfk8A3of61xRcnfAU8P0pz2MtW3hI6V9vakX7QczmhElNLTiG8ZCZBSm32ZCAh5SFHgfFZB1ommnnA99Yiz7s09xKHcZBRiu1iHpVKBzNSAace3rlQhZALZCjlFq91ZABLoxITmegBXAvZBW5g5RdhnqHvZCq7B6Xm9luySQp5KhTHJWGhktdIZD";
         die;
     }
     
